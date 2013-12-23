3f307384f70987d4199f33c7b01dc2936bab8644
cftyn <cftyn1@gmail.com>
2013-12-23 22:15:57 +0800

    switch 兩端 host 可以 ping 通，可以在 forwarding 之前 insert flow entry (從修改 insert flow entry 的 Priority 確定) 使用default Dijkstra路徑，尚未具備 loadbalance 能力 感謝 loadbalancer 的 code 的努力

---
+++ ILoadbalanceRoutingService.java
@@ -0,0 +1,23 @@
+package net.floodlightcontroller.ActiveLB;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.routing.Route;
+
+public interface ILoadbalanceRoutingService extends IFloodlightService  {
+
+	public Route getRoute(long srcId, short srcPort,
+            long dstId, short dstPort, long cookie);
+
+}
